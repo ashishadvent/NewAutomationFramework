2d8dd4f30fc4ae20face239e4ce2ee44d2cf17a2
akesarwa <akesarwa@NOI-5ZHVR72.gemalto.com>
2019-04-14 14:18:46 +0530

    Second Update related to Log4j and BaseClass.java file

--- src/test/java/com/flipkart/pageobjects/BaseClass.java
+++ src/test/java/com/flipkart/pageobjects/BaseClass.java
@@ -3,10 +3,54 @@
  */
 package com.flipkart.pageobjects;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import com.flipkart.utilities.ReadConfig;
+
 /**
  * @author ashish
  *
  */
 public class BaseClass {
-
+	//to create reference variable of ReadConfig Class to access variable declared in config.properties file through its methods....
+	ReadConfig config=new ReadConfig();
+	
+	//getting the values from config.properties file
+	public String testURL=config.getApplicationURL();
+	public String userName=config.getUserName();
+	public String password=config.getPassword();
+	public String chromeDriverPath=config.getchromeDriverPath();
+	public String IEDriverPath=config.getIEDriverPath();
+	public String FFDriverPath=config.geckoDriverPath();
+	
+	//create public reference variable of WebDriver class
+   public WebDriver driver;	
+   @Parameters("browser")
+   @BeforeClass 
+   void setUp(String br) {
+	    Logger logger=Logger.getLogger("BaseClass");
+	    PropertyConfigurator.configure("Log4j.properties");
+	   
+	   if (br.toLowerCase().equals("chrome")) {
+		   System.setProperty("webdriver.chorme.driver",config.getchromeDriverPath());
+		   driver=new ChromeDriver();
+	   }
+	   
+	   else if(br.toLowerCase().equals("firefox")) {
+		   System.setProperty("webdriver.gecko.driver",config.geckoDriverPath());
+		   driver=new FirefoxDriver();
+	   }
+	   else if(br.toLowerCase().equals("ie")) {
+		   System.setProperty("webdriver.ie.driver",config.getIEDriverPath());
+		   driver=new InternetExplorerDriver();
+	   }
+	   
+   }
 }
--- src/test/java/com/flipkart/utilities/ReadConfig.java
+++ src/test/java/com/flipkart/utilities/ReadConfig.java
@@ -3,11 +3,56 @@
  */
 package com.flipkart.utilities;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 /**
  * @author ashish
  *
  */
 public class ReadConfig {
 	
-
+    Properties property;
+   public ReadConfig(){
+    try {
+    	File file=new File("./Configuration/config.properties");
+    	FileInputStream inStream=new FileInputStream(file);
+    	property.load(inStream);
+    }catch(Exception e) {
+    	e.getStackTrace();
+    }
+   }
+    public String getApplicationURL() {
+    	String url= property.getProperty("testURL");
+		return url;
+    	
+    }
+    public String getUserName() {
+    	String uname= property.getProperty("userName");
+		return uname;
+    	
+    }
+    public String getPassword() {
+    	String pwd= property.getProperty("password");
+		return pwd;
+    	
+    }
+    public String getchromeDriverPath() {
+    	String chromepath= property.getProperty("chormeDriverPath");
+		return chromepath;
+    	
+    }
+    
+    public String getIEDriverPath() {
+    	String IEpath= property.getProperty("IEDriverPath");
+		return IEpath;
+    	
+    }
+    
+    public String geckoDriverPath() {
+    	String ffpath= property.getProperty("geckodriverPath");
+		return ffpath;
+    	
+    }
 }
